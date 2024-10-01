448f992a36d86e5caacee119005e95c637c078e4
Jeremy Kwagh <kwagh.jeremiah@gmail.com>
2024-10-01 15:46:21 -0400

    added Go connections and connected to postgresql database

--- server/main.go
+++ server/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"database/sql"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
@@ -22,6 +26,15 @@ func main() {
 		log.Fatal("DB_URL is not found in the environment")
 	}
 
+	conn, err := sql.Open("postgres", dbUrl)
+	if err != nil {
+		log.Fatal("Can't connect to database", err)
+	}
+
+	apiCfg := apiConfig{
+		DB: database.New(conn),
+	}
+
 	router := chi.NewRouter() //Create router using chi
 
 	//cors config to let browser make requests
@@ -33,4 +46,19 @@ func main() {
 		AllowCredentials: true,
 		MaxAge:           300,
 	}))
+
+	v1Router := chi.NewRouter()
+
+	v1Router.Get("ready", handlerReadiness)
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server starting on port %v", portString)
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
